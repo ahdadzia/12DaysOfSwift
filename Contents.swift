46426c2a72ea9eda78c385d67d6c2b1fd59096ab
ahdadzia <ahda.fakhri@gmail.com>
2021-03-16 13:37:37 +0700

    Nyoba variabel string, multi-string, dan integer

--- Contents.swift
+++ Contents.swift
@@ -1,3 +1,24 @@
 import UIKit
 
-var str = "Hello, playground"
+
+var name = "ahda"
+name = "dzia"
+
+// tipe data integer
+var age = 15
+age = 16
+
+var bglPrice = 1_700_000
+
+var bgl = "harga bgl \(bglPrice)"
+
+// multiline string
+var quotes = """
+
+ "Memaafkan belum tentu membuat kita lebih baik atau bahkan merasa lebih baik,
+  tetapi yang jelas membuka jalan kebaikan."
+        - Eleanor Roosevelt
+
+"""
+
+print(quotes)
